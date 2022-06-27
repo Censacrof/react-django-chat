1dc1a3d2423e4f3923c32e0df90cc872f087474d
Francesco Galisi <f.galisi@datawizard.it>
2022-06-28 01:32:57 +0200

    Added autoscroll (to fix)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import './style/App.scss';
 import 'classnames'
 import classNames from 'classnames';
-import { useCallback, useState } from 'react';
+import { useRef, useCallback, useState } from 'react';
 
-function NewMessageText(props) {
-  const [value, setValue] = useState('')
-
-  const onChange = useCallback((event) => {
-    setValue(event.target.value)
-  })
+function NewMessageTextArea(props) {
+  const maxChars = 255;
 
   return (
     <div className="row new-message-row">
       <div className="col new-message-col">
-        <textarea onChange={onChange} value={value} placeholder="Scrivi un messaggio..."></textarea>
-        <span className="counter">{value.length}/255</span>
+        <textarea ref={props.textAreaRef} value={props.value} placeholder="Scrivi un messaggio..." onChange={(event) => {
+          let newValue = event.target.value.substring(0, maxChars)
+          props.setValue(newValue)
+        }}></textarea>
+        <span className="counter">{props.value.length}/{maxChars}</span>
       </div>
     </div>
   )
 }
 
-function Message({message}) {
+function Message({message, currentSender, innerRef}) {
   return (
     <div className={classNames(
       'row',
       'message-row',
-      {'own': message.own}
+      {'own': message.sender == currentSender}
     )}>
-      <div className="col-9 message-col">
+      <div ref={innerRef} className="col-9 message-col">
         <div className="row">
           <div className="col sender">
             <span>{message.sender}</span>
@@ -49,15 +48,36 @@ function Message({message}) {
 }
 
 function Chat() {
-  const createMessageData = (id, sender, text, own) => {
-    return {id: id, sender: sender, text: text, date: '23 aprile 2022 | 10:53', own: own}
+  const currentSender = 'Giovanni'
+
+  const createMessageData = (id, sender, text) => {
+    return {id: id, sender: sender, text: text, date: '23 aprile 2022 | 10:53'}
   }
 
-  const messages = [
-    createMessageData(0, 'Aldo', 'Vaffangulooooo!', false),
-    createMessageData(1, 'Giovanni', 'We sta calmino eh', true),
-    createMessageData(2, 'Giacomo', 'Ma che ohhhhh', false),
-  ]
+  const [messages, setMessages] = useState([
+    createMessageData(0, 'Aldo', 'Vaffangulooooo!'),
+    createMessageData(1, 'Giovanni', 'We sta calmino eh'),
+    createMessageData(2, 'Giacomo', 'Ma che ohhhhh'),
+  ])
+
+  const [newMessageText, setNewMessageText] = useState('')
+
+  const newMessageTextAreaRef = useRef()
+  const messageListRef = useRef()
+
+  const sendMessageButtonClick = useCallback((event) => {
+    let msg = createMessageData(
+      Math.floor(Math.random() * 99999999),
+      currentSender,
+      newMessageText,
+    )
+    let newMessages = messages.concat(msg)
+    
+    setNewMessageText('')
+    setMessages(newMessages)
+    newMessageTextAreaRef.current.focus()
+    messageListRef.current.scrollTop = messageListRef.current.scrollHeight
+  })
 
   return (
     <div className="row chat">
@@ -68,20 +88,20 @@ function Chat() {
           </div>
         </div>
         <div className="row message-list-row">
-          <div className="col message-list-col">
+          <div ref={messageListRef} className="col message-list-col">
             {
               messages.map((message) => {
                 return (
-                  <Message key={message.id} message={message} />
+                  <Message key={message.id} message={message}  currentSender={currentSender} />
                 )
               })
             }
           </div>
         </div>
-        <NewMessageText />
+        <NewMessageTextArea textAreaRef={newMessageTextAreaRef} value={newMessageText} setValue={setNewMessageText}/>
         <div className="row send-message-row">
           <div className="col send-message-col">
-            <button>Invia</button>
+            <button onClick={sendMessageButtonClick}>Invia</button>
           </div>
         </div>
       </div>
