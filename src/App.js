83b5829cbef5258ad5a9aa2e58bb72fa0f1d251c
Francesco Galisi <f.galisi@datawizard.it>
2022-06-27 19:23:36 +0200

    Create chat component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/App.scss';
+import 'classnames'
+import classNames from 'classnames';
+
+function Message({message}) {
+  return (
+    <div className={classNames(
+      'row',
+      'message-row',
+      {'own': message.own}
+    )}>
+      <div className="col-9 message-col">
+        <div className="row">
+          <div className="col sender">
+            <span>{message.sender}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text">
+            <span>{message.text}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col date">
+            <span>{message.date}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Chat() {
+  const createMessageData = (id, sender, text, own) => {
+    return {id: id, sender: sender, text: text, date: '23 aprile 2022 | 10:53', own: own}
+  }
+
+  const messages = [
+    createMessageData(0, 'Aldo', 'Vaffangulooooo!', false),
+    createMessageData(1, 'Giovanni', 'We sta calmino eh', true),
+    createMessageData(2, 'Giacomo', 'Ma che ohhhhh', false),
+  ]
+
+  return (
+    <div className="row chat">
+      <div className="col">
+        <div className="header row">
+          <div className="col">
+            <span>Prosalute</span>
+          </div>
+        </div>
+        <div className="message-list">
+          {
+            messages.map((message) => {
+              return (
+                <Message key={message.id} message={message} />
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Chat />
     </div>
   );
 }
