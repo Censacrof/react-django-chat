7777986051c4bc64fff2570e32cdbf76233eda1a
Francesco Galisi <f.galisi@datawizard.it>
2022-06-29 17:15:58 +0200

    Added related name

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE, null=False)
     text = models.CharField(max_length=1024, null=False)
     date = models.DateTimeField(default=datetime.now, null=False)
