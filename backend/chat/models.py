f4aae7cdcfe7f865818a583341f9d7dc66b4297c
Francesco Galisi <f.galisi@datawizard.it>
2022-06-29 17:03:08 +0200

    Created chat app

---
+++ backend/chat/models.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from statistics import mode
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    text = models.CharField(max_length=1024, null=False)
+    date = models.DateTimeField(default=datetime.now, null=False)
