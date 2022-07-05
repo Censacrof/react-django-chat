c0770f968a7fb33d4bccafc41bb93261c84da166
Francesco Galisi <f.galisi@datawizard.it>
2022-07-05 11:49:02 +0200

    Added view CurrentUserIfo

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
+from django.http import Http404, HttpResponseForbidden
 from django.urls import path, include
 from django.contrib.auth.models import User
 from django.contrib import admin
 from rest_framework import routers, viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.response import Response
 
 from chat.views import MessageViewSet
 from backend.serializers import UserSerializer
@@ -12,10 +14,21 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class CurrentUserInfoViewSet(viewsets.ViewSet):
+    def list(self, request):
+        if not request.user.is_authenticated:
+            raise Http404()
+        
+        queryset = User.objects.get(pk=request.user.id)
+        serializer = UserSerializer(queryset, context={'request': request})
+        return Response(serializer.data)
+    
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'message', MessageViewSet, basename='message')
+router.register(r'current-user-info', CurrentUserInfoViewSet, basename='current-user-info')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
