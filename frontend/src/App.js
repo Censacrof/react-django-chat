0a3db90de880a48a242a472ad3a3c86c813ad0de
Francesco Galisi <f.galisi@datawizard.it>
2022-07-15 02:33:16 +0200

    Frontend fresh start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import './style/App.scss'
-import Login from './Login';
-import Chat from './Chat'
 import 'classnames'
-import { getApiInstance } from './Api';
 import { useState } from 'react';
 
 function App() {
-  const api = getApiInstance()
-
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  api.onLogin(() => {
-    setIsLoggedIn(true)
-    console.log(api)
-  })
 
   return (
     <div className="App container">
-      {isLoggedIn 
-        ? (<Chat />)
-        : (<Login />)
-      }
     </div>
   );
 }
